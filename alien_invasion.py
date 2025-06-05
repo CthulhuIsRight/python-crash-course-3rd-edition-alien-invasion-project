d2f5aa7c979f110259cdb5d21d507e83801963fb
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-06-05 16:42:07 -0700

    Add Ship instance to alien_invasion.py and draw it on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """
@@ -16,6 +17,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
     
     def run_game(self):
         """
@@ -28,6 +30,8 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible
             pygame.display.flip()
             self.clock.tick(60)
