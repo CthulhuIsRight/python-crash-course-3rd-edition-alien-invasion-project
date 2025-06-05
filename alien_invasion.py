0a9cecf71c7f604bbdc8b16b890fd74c05e104ff
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-06-04 21:49:14 -0700

    Added settings.py file and refactored alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """
     Overall class to manage game assets and behavior.
@@ -11,10 +13,9 @@ class AlienInvasion:
         """
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        # Set the background color
-        self.bg_color = (230, 230, 230)
     
     def run_game(self):
         """
@@ -25,9 +26,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
             # Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # Make the most recently drawn screen visible
             pygame.display.flip()
             self.clock.tick(60)
