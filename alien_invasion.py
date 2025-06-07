65392a71d221aa5ace9911bbe5db76baf94c8425
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-06-07 11:52:01 -0700

    Added code for movement of ship to right of screen, do note that this movement is not continious when the button is held down

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 10
 
     def _update_screen(self):
         """
