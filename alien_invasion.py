ffa1b28667b1c3f72092bbbe587b3918dd1bb76d
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-06-09 12:06:34 -0700

    Implemented rightward movement for the entire alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -119,6 +120,12 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _update_aliens(self):
+        """
+        Update the positions of all aliens in the fleet.
+        """
+        self.aliens.update()
 
     def _update_screen(self):
         """
