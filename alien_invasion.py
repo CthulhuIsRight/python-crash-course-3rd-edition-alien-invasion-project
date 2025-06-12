1165631c5d8f66eedb7de219bd5b9545de2c1ccf
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-06-12 00:51:33 -0700

    Implemented functionality to track/display the highest score achieved in the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -147,6 +147,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
         if not self.aliens:
             # Destry existing bullets and create a new fleet
             self.bullets.empty()
