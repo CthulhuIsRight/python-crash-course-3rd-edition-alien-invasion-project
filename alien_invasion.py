6ca4df774592f29e82ab579655f296b9b1814e40
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-06-12 01:06:31 -0700

    Added functionality to display level player is currently on

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,7 @@ class AlienInvasion:
             self.settings.initalize_dynamic_settings()
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
             self.game_active = True
 
             # Get rid of any remaning bullets and aliens
@@ -153,6 +154,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         """
